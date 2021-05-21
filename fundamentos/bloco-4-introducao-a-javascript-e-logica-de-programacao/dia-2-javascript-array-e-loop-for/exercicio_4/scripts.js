d37b5f1d8aecbd1ba8d0be2e77639e25091b737e
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-21 12:23:12 -0300

    execucao do item 9 - imprimir o resultado da divisao de cada um dos elementos do array por 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4/scripts.js
@@ -92,3 +92,9 @@ for ( let index = 0 ; index < 25 ; index += 1) {
 }
 console.log(numbersTo25);
 
+// Utilizando o array acima imprima o resultado da divisão de cada um dos elementos por 2
+
+for (let index = 0 ; index < numbersTo25.length ; index += 1) {
+  let division = numbersTo25[index] / 2
+  console.log(division);
+}
