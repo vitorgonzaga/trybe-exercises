f2cf76391ecd1fb2dc61840b31aab00f548424b9
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-21 11:11:54 -0300

    Execuçao da parte 4 do exercicio - if/else para imprimir mensagem

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4/scripts.js
@@ -21,4 +21,10 @@ console.log(sum);
 let media = (sum / numbers.length);
 console.log(media);
 
-//
+// Caso o resultado da média seja maior que 20 imprima a mensagem "valor maior que 20". Caso não seja imprima a mensagem "valor menor que 20"
+
+if (media > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor que 20");
+}
