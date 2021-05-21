d973cd8828b15e5584a1f35d217815e10d3a8d9d
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-21 10:29:58 -0300

    Execuçao do exercicios do conteudo ref aos arrays - 1, 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_1/scripts.js
@@ -0,0 +1,14 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// Obtenha o valor 'Serviços'
+let menuServices = menu[1];
+console.log(menuServices);
+
+// Procure o índice do valor 'Portfólio'
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+console.log(indexOfPortfolio);
+
+// Adicione o valor 'Contato' no final do array
+
+menu.push('Contato');
+console.log(menu);
