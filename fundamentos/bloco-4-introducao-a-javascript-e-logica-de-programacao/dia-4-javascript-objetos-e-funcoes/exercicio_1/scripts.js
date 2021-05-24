3b07c46808bc74daac2d5cd34feeb3da3295f3c3
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-24 12:15:03 -0300

    Iniciando os exercicios do bloco 4 dia 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_1/scripts.js
@@ -0,0 +1,14 @@
+let car = {
+  type: 'Fiat',
+  model: '500',
+  color: 'white',
+};
+
+let plater = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2, silver: 3
+  }
+}
