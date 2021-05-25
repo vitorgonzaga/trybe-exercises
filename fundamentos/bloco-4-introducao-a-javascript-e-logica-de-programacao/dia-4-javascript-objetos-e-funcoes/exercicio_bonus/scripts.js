0f5a59ee702bd79483ac794a1473e6078470004b
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-25 02:32:10 -0300

    exercicio 3 bonus bloco 4.4 - Gabarito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_bonus/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_bonus/scripts.js
@@ -72,3 +72,102 @@ function arrayOfNumbers(myArray) {
 
 
 console.log(arrayOfNumbers(vector));
+
+// 3.A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função. Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+// Gabarito
+
+const result = {};
+
+for (let index = 0; index < basket.length; index += 1) {
+  const fruit = basket[index];
+  if (!result[fruit]) {
+    result[fruit] = 0;
+  }
+  result[fruit] += 1  
+}
+
+console.log(result);
+
+const summaries = [];
+for (fruit in result) {
+  let message = `${result[fruit]} ${fruit}`;
+  if (result[fruit] > 1) {
+    message += 's';
+  }
+  summaries.push(message);
+}
+
+console.log(summaries);
+
+console.log(`Sua cesta possui: ${summaries.join(', ')}.`);
+
+
+// basket.sort()
+// console.log(basket)
+
+// let clearArray = [];
+// let arrContagem = [];
+
+// for (let index = 0; index < basket.length; index += 1) {
+//   let strFruit = basket[index];
+//   let intContador = 0;
+//   for (let i = 0; i < basket.length; i += 1) {
+//     let strFruitVerified = basket[i];
+//     if (strFruit == strFruitVerified) {
+//       intContador += 1;      
+//     } 
+//   }
+  
+//   let blnAddNewFruit = false;
+
+//   if (clearArray.length > 0) {
+//     for (let u = 0; u < clearArray.length; u += 1) {
+//       if (strFruit == clearArray[u]) { // Nesse caso não precisa incluir nova Fruta no clear array
+//         // blnCheckElement = true;
+//         break;
+//       } else {
+//         blnAddNewFruit = true;
+//       }      
+//     }
+//     if (blnAddNewFruit == true) {
+//       clearArray.push(strFruit);
+//       arrContagem.push(intContador);      
+//     }
+//   } else {
+//     clearArray.push(strFruit);
+//     arrContagem.push(intContador);
+//   }  
+// }
+
+// console.log(clearArray);
+// console.log(arrContagem);
+
+// const cesta = {};
+
+// for (let key of clearArray) {
+//   cesta[key] = arrContagem[key];
+// }
+
+// console.log(cesta);
