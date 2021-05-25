bd3b56fd093522a14ecf43d62f9c2c9574065e84
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-24 21:21:29 -0300

    execucao do item 6 da parte 2 do exercicio ref a funcoes do bloco 4.4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_4/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_4/scripts.js
@@ -126,3 +126,16 @@ function verificaQuemMaisSeRepete(myArray) {
 
 console.log(verificaQuemMaisSeRepete(arrTeste));
 
+// 6. Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
+let n = 5;
+
+function somaDeHumAteN(integer) {
+  let total = 0;
+  for (let index = 1; index <= integer; index += 1) {
+    total += index
+  }
+    return total
+}
+
+console.log(somaDeHumAteN(n));
