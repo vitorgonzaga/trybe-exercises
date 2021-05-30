d41fda138e7f93e30ecc00cc66bc293f846edc77
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-30 17:26:34 -0300

    realizacao do item 5 do exercicio - entendi o conceito do targetgit add .! Finalmentegit add .

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio_1/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio_1/main.js
@@ -48,6 +48,13 @@ myWebpage.addEventListener('dblclick', openMyPortfolio);
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
+function changeColorText(objeto){
+  console.log(objeto.target);  
+  objeto.target.style.color = 'yellow';
+}
+
+myWebpage.addEventListener('mouseover', changeColorText);
+
 // Segue abaixo um exemplo do uso de event.target:
 
 
