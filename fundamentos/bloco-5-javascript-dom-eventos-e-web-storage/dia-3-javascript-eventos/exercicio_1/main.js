299195329a3831a3d42de7d9f836a3072b02ab35
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-05-30 16:41:09 -0300

    realizacao dos itens 2, 2.1 e 3 do exercicio

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio_1/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio_1/main.js
@@ -5,19 +5,45 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
- Copie esse arquivo e edite apenas ele;
- Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
- Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
- Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-1. Que tal redirecionar para seu portifólio?
- Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
+1. Copie esse arquivo e edite apenas ele;
+2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+2.1 Deve existir apenas um elemento com a classe 'tech'. Como você faz isso? 
 */
+
+function addClassTech (elemento) {
+  
+  let arrElementsWithClassTech = document.querySelectorAll('.tech');
+  
+  if (arrElementsWithClassTech.length >= 0) {
+    // blnThereIsTechClass = true
+    console.log('Não é permitido adicionar a classe "tech" em mais de 1 elemento');
+  } else {
+    // (blnThereIsTechClass == false)
+    elemento.className = 'tech'
+  }
+}
+
+/*
+3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech';
+*/
+let elementTechClass = document.querySelector('.tech');  
+
+function ChangeBackgroundColorOfTech() {    
+  elementTechClass.style.backgroundColor = '#fff';
+}
+input.addEventListener('change', ChangeBackgroundColorOfTech);
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+
+// 4.1 Que tal redirecionar para seu portifólio?
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
