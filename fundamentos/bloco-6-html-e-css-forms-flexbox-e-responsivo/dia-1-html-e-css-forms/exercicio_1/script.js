709f9205d0c14c6cd62ee37c0c21c21974c955df
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-06-12 00:04:15 -0300

    finalizacao da procedure ref. a rotina de validacoes do form exercico bloco 6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/exercicio_1/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/exercicio_1/script.js
@@ -87,7 +87,7 @@ function fnValidationPadrao (elemInput, strName) {
 // **********************************************************************************************************************************
 function fnDateValidation(elemInput, strName) {
   // Primeiro verifica se o campo foi preenchido;
-  if (elemInput.value.length === 0) {
+  if (elemInput.value.length === 0 || elemInput == null || elemInput == undefined) {
     return {
       message: 'A data não foi informada'
     }
@@ -110,33 +110,163 @@ function fnDateValidation(elemInput, strName) {
 // Função para validacao das tags select.
 // **********************************************************************************************************************************
 function fnSelectValidation(elemInput, strName) {
-  // A variavel 
+  // A variavel elemInput nesse caso é um document ref. a tag select  
+  let option = elemInput.options[elemInput.selectedIndex];
+  if (inputs[strName].required && (!option || option.disabled)) {
+    return false;
+  }
+  return true;
 }
+
+// **********************************************************************************************************************************
+// Função para validacao dos inputs type radio.
+// **********************************************************************************************************************************
+function fnRadioValidation (elemInput, strName) {
+  let checked = document.querySelector(`[name=${strName}]:checked`);
+
+  if (checked === null) {
+    return false;
+  }
+  return true;
+}
+
+// Declara um objeto/dictionary, uma especie de de-para para os diferentes tipos de input
+// existem os inputs tipo texto, select, radio e date, logo, a declaracao do objeto poderá ficar assim:  
+let dicValidationStrategies = {
+  default: fnValidationPadrao,
+  date: fnDateValidation,
+  select: fnSelectValidation,
+  radio: fnRadioValidation
+}  
 
 
 function fnStartValidation() {
-// Declara um objeto/dictionary, uma especie de de-para para os diferentes tipos de input
-// existem os inputs tipo texto, select, radio e date, logo, a declaracao do objeto poderá ficar assim:  
-  let dicValidationStrategies = {
-    default: fnValidationPadrao,
-    date: fnDateValidation,
-    select: fnSelectValidation,
-    radio: fnRadioValidation
-  }  
+  
+  // Declara objeto/dictionary vazio para preenchimento com os possíveis logs de erro;
+  let validationList = {};
   // Percorrer o objeto iteravel inputs identificando o tipo de cada elemento para enfim chamar a funcao de validacao por tipo de input
   // utilizando um loop for/in no objeto inputs declarado mais acima
-  for (let inputName in inputs)  {
-    let elemInput = document.querySelector('[name=${inputName}]');
+  for (let inputName in inputs)  { 
     let strInputType = inputs[inputName].type;
+    let elemInput = document.querySelector(`[name=${inputName}]`);    
     // Se existir a propriedade type no objeto inputs declarado lá em cima chama a função correspondente ao match no dictionary
     // declarado dentro dessa função
     if (strInputType) {
       let strValidationFunctionByType = dicValidationStrategies[strInputType];
-      return strValidationFunctionByType(elemInput, inputName);
+      varResult = strValidationFunctionByType(elemInput, inputName);
+      // return blnResult
+      validationList[inputName] = varResult;
     }     
-    return dicValidationStrategies.default(elemInput, inputName);
-  }    
+    varResult = dicValidationStrategies.default(elemInput, inputName);
+    // return 
+    validationList[inputName] = varResult;
+  }
+  
+  // Após concluir o for que percorreu todos os elementos do form, realiza o for do objeto validationList;
+  let counter = 0;
+  let messages = [];
 
+  for (let index in validationList) {
+    // Contando quantos erros foram encontrados;
+    if (validationList[index] === false) {
+      counter += 1;
+    }
+    // verificando se, além de valores true or false (booleanos), há mensagem de erro. Caso encontre, soma no contador;
+    if (validationList[index].message) {
+      counter += 1;
+      messages.push(validationList[index].message);
+    }
+  }  
+  return {
+    ErrorQtd: counter, messages,
+  }
+}
+
+function fnLimpaDivs(){
+  let errorDivs = document.querySelectorAll('.error');
+  for (let div of errorDivs) {
+    div.remove();
+  }
+  let dataDiv = document.querySelector('.data');
+  if(dataDiv){
+    dataDiv.remove();
+  }
+}
+
+function renderErrorMessages(messages){
+  // Appenda uma div com class='error' para cada mensagem verificada na função fnStartValidation;
+  let form = document.querySelector('#cv-form');
+  let messageDiv = document.createElement('div');
+  messageDiv.className = 'error';
+  form.prepend(messageDiv);
+
+  for(let message of messages){
+    let p = document.createElement('p');
+    p.innerText = message;
+
+    messageDiv.appendChild(p);
+  }
+}
+
+// ******************************************************************************************************
+// INICIO DE FUNCOES DESTINADAS AO PREENCHIMENTO DA DIV QUE CONSOLIDA OS INPUTS REALIZADOS NO FORMULARIO
+// ******************************************************************************************************
+function defaultRendering(input){
+  let p = document.createElement('p');
+  p.innerText = input.value;
+
+  return p;
+}
+// ***************************************
+function radioRendering(input){
+  let p = document.createElement('p');
+  let name = input.getAttribute('name');
+  let checked = document.querySelector(`[name=${name}]:checked`);
+
+  if(checked){
+    p.innerText = checked.value;
+  }
+
+  return p;
+}
+// ***************************************
+function selectRendering(input){
+  let p = document.createElement('p');
+  let option = getSelectedOption(input)
+  p.innerText = option.value;
+  
+  return p;
+}
+// ***************************************
+
+let renderStrategies = {
+  default: defaultRendering,
+  radio: radioRendering,
+  select: selectRendering,
+}
+
+function renderData(){
+  
+  let dataDiv = document.createElement('div');
+  dataDiv.className = 'data';
+
+  let form = document.querySelector('#cv-form');
+  form.prepend(dataDiv);
+
+  for(let name in inputs){
+    let inputType = inputs[name].type;
+    let input = document.querySelector(`[name=${name}]`);
+
+    let element;
+
+    if(renderStrategies[inputType]){
+      element = renderStrategies[inputType](input, dataDiv)
+    } else {
+      element = renderStrategies.default(input, dataDiv)
+    }
+
+    dataDiv.appendChild(element);
+  }
 }
 
 function fnHandleSubmit(event) {
@@ -144,11 +274,40 @@ function fnHandleSubmit(event) {
   // Isso é necessário para que procedimentos sejam realizados antes desse comportamento padrão;
   // Essa é a funcionalidade do preventDefault;
   event.preventDefault();
-  fnStartValidation();
+  // inicia rotina de verificacao de cada item declarado no objeto (chave:valor) no início do código;
+  // O resulto será uma contagem de logs
+  let varResultOfValidation = fnStartValidation();
+  // Limpa a div anterior resultado da renderizacao do preenchimento anteior;
+  fnLimpaDivs();
+
+  if (varResultOfValidation.ErrorQtd === 0) {
+    renderData();
+  } else {
+    varResultOfValidation.messages.unshift('Dados Inválidos');
+  }
+  renderErrorMessages(varResultOfValidation.messages)
+}
+
+function clearFiels(){
+  let formElements = document.querySelectorAll('input');
+  let textArea = document.querySelector('textarea');
+  let div = document.querySelectorAll('.div-curriculum') // Acho que essa linha está sobrando, pois não há nenhuma div com essa classe;
+  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+    formElements[index].value = '';
+    textArea.value = '';
+    div[index].innerText = ''; // Usou innerText ao invés de innerHTML porque o retorno do querySelectorAll é nodeList ao inves de HTMLdocument.
+  }
 }
 
 window.onload = function() {
- fnCreateStateOptions();
- let btnSubmit = document.querySelector('#submit-button');
- btnSubmit.addEventListener('click', fnHandleSubmit);
+  
+  fnCreateStateOptions();
+  
+  let btnSubmit = document.querySelector('#submit-button');
+  btnSubmit.addEventListener('click', fnHandleSubmit);
+
+  let clearButton = document.querySelector('#clear-button');
+  clearButton.addEventListener('click', clearFiels);
+
+
 }
