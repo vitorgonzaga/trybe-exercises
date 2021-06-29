ecbd651125b1d35d3d3858297dcda335fc45a410
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-06-29 19:21:08 -0300

    finalizando array destrutucturing

--- fundamentos/bloco-8-higher-order-functions-do-javascript-ES6/dia-5-Hofs-spread operator-rest-destructuring/training.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-ES6/dia-5-Hofs-spread operator-rest-destructuring/training.js
@@ -97,7 +97,66 @@ printProductDetails(newProduct); // Promoção! Smart TV Crystal UHD por apenas 
 // ARRAY Destructuring
 // ******************************************************************
 
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
 
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada
 
+// Para Fixar
+// 1 - Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
 
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [strSaudacao, fnSaudacao] = saudacoes;
+
+console.log(strSaudacao);
+
+// 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+// ***************************************************************************************************
+// Solução que eu pensei...
+// const animais = ['gato', 'água', 'arroz'];
+// const [animal, bebida, comida] = animais;
+
+// console.log(comida, animal, bebida); // arroz gato água
+// ***************************************************************************************************
+
+// ***************************************************************************************************
+// Gabarito
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+// ***************************************************************************************************
+
+[comida, animal, bebida] = [bebida, comida, animal]; // Criando o array e o desestruturando
+console.log(comida, animal, bebida); // arroz gato água
+
+// 3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares .
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,, ...numerosPares] = numerosPares
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Poderia fazer um map:
+
+const extractEvenFromArray = (array) => array.map(item => item % 2 === 0 ? item : '')
+
+console.log(extractEvenFromArray(numerosPares));
 
