deea0089402adfab07742721a953f2a067bbcf05
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-06-29 20:18:58 -0300

    finalizando o training para iniciar os exercicios

--- fundamentos/bloco-8-higher-order-functions-do-javascript-ES6/dia-5-Hofs-spread operator-rest-destructuring/training.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-ES6/dia-5-Hofs-spread operator-rest-destructuring/training.js
@@ -184,7 +184,17 @@ console.log(getNationality(otherPerson)); // Ivan is Russian
 console.log(getNationality(person));
 
 // ******************************************************************
-// Default Destructuring
+// Default Parameters
+// ******************************************************************
+
+// Para praticar, escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
+
+const multiply = (number, value = 1) => (number * value);
+
+console.log(multiply(8));
+
+// ******************************************************************
+// Object Property Shorthand
 // ******************************************************************
 
 // Agora é hora de praticar: altere a função getPosition utilizando a property shorthand .
