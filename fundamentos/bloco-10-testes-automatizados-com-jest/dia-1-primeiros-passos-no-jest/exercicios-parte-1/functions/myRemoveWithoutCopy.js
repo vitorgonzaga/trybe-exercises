e19b8f2cf5081531da73ceaf564c3c88f82aa45f
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-07-14 18:57:06 -0300

    exercise 3 function myRemoveWithoutCopy

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercicios-parte-1/functions/myRemoveWithoutCopy.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercicios-parte-1/functions/myRemoveWithoutCopy.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 
 function myRemoveWithoutCopy(arr, item) {
   for (let index = 0, len = arr.length; index < len; index += 1) {
@@ -12,11 +12,13 @@ function myRemoveWithoutCopy(arr, item) {
   return arr;
 }
 
+module.exports = myRemoveWithoutCopy;
+
 // implemente seus testes aqui
 
-assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
-assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
-const myArray = [1, 2, 3, 4];
-myRemoveWithoutCopy(myArray, 3);
-assert.deepStrictEqual(myArray, [1, 2, 4]);
-assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+// assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+// const myArray = [1, 2, 3, 4];
+// myRemoveWithoutCopy(myArray, 3);
+// assert.deepStrictEqual(myArray, [1, 2, 4]);
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
