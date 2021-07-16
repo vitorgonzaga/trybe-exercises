a0d38e32f5c779915c6cf13e0f40de052675511d
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-07-15 21:54:25 -0300

    exercise 6.1 function getAnimal by name

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exercicios/functions/animalFunctions.js
@@ -0,0 +1,42 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalsByType = (type) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.filter((animal) => animal.type === type);
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      };
+
+      return reject({ error: 'Não possui esse tipo de animal.' });
+    }, 100);
+  })
+  );
+
+const getListAnimals = (type) => (
+  findAnimalsByType(type).then(list => list)
+  );
+
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(Animals.find(({ name : animalName }) => name === animalName));
+      reject ('Nenhum animal com esse nome!');
+    }, 200);
+  })
+);
+
+const getAnimal = (name) => {
+  return findAnimalByName(name).then(data => data)
+};
+
+module.exports = {
+  findAnimalsByType,
+  findAnimalByName,
+  getListAnimals,
+  getAnimal,
+}
---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exercicios/tests/animalFunctions.test.js
@@ -0,0 +1,21 @@
+const animalFunctions = require('../functions/animalFunctions');
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      // expect.assertions(1);
+      return animalFunctions.getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      // expect.assertions(1);
+      return animalFunctions.getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
